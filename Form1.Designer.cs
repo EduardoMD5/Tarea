8e62ce4ec206971ecae2eb16d5ee06e58becf4e5
EduardoMD5 <48935111+EduardoMD5@users.noreply.github.com>
2019-03-25 19:39:09 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Punto_de_venta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.producto_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.producto_nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.producto_precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.producto_url = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.producto_id,
+            this.producto_nombre,
+            this.producto_precio,
+            this.producto_url});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 295);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // producto_id
+            // 
+            this.producto_id.HeaderText = "producto_id";
+            this.producto_id.Name = "producto_id";
+            this.producto_id.ReadOnly = true;
+            // 
+            // producto_nombre
+            // 
+            this.producto_nombre.HeaderText = "producto_nombre";
+            this.producto_nombre.Name = "producto_nombre";
+            this.producto_nombre.ReadOnly = true;
+            // 
+            // producto_precio
+            // 
+            this.producto_precio.HeaderText = "producto_precio";
+            this.producto_precio.Name = "producto_precio";
+            this.producto_precio.ReadOnly = true;
+            // 
+            // producto_url
+            // 
+            this.producto_url.HeaderText = "producto_url";
+            this.producto_url.Name = "producto_url";
+            this.producto_url.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(803, 470);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn producto_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn producto_nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn producto_precio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn producto_url;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Punto_de_venta
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection mySqlConnecion;
+        MySqlCommand mySqlCommand;
+        MySqlDataReader reader;
+
+        public Form1()
+        {       
+            InitializeComponent();
+        }
+
+        private void cargadatos()
+        {
+            try
+            {
+                dataGridView1.Rows.Clear();
+                mySqlConnecion = new MySqlConnection("host=localhost;user=root;password=1234;database=punto_de_venta;"); // String de connexion
+                mySqlConnecion.Open();
+                mySqlCommand = new MySqlCommand("SELECT * FROM productos", mySqlConnecion);
+                reader = mySqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(reader.GetString(0), (reader.GetString(1)), (reader.GetString(2)), (reader.GetString(3)));
+                }
+                mySqlConnecion.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargadatos();
+        }
+    }
+}
